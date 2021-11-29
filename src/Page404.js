a4a1e7270d25527c037bd8bd89fa3de2ad897946
JYG <jojeayun900@gmail.com>
2021-11-29 18:07:52 +0900

    modify text

--- src/Page404.js
+++ src/Page404.js
@@ -4,11 +4,11 @@ function Page404() {
   return (
 
     //여기부터 JSX
-    <div className="App">
+    <main>
         <img className="logo_404" alt="MAT로고" src="logo_404.png" />
         <img className="icon_404" alt="MAT로고" src="icon_404.png" />
-        <p className="errortext">페이지를 찾지 못했어요 :(<br />입력하신 주소가 정확한지 한번 더 확인해주세요</p>
-    </div>
+        <p className="errortext">페이지를 불러오지 못했어요 :(<br />서버 점검중이거나 입력하신 주소가 잘못되었습니다</p>
+    </main>
     //여기까지 JSX
   );
 }
