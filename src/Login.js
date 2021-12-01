b8220d5483da74cd63ee1ffb430543ba65d00765
JYG <jojeayun900@gmail.com>
2021-12-01 21:18:31 +0900

    make login fuction

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,60 @@
 import './App.css';
 import { createPortal } from "react-dom";
+import { Component } from 'react';
 
-function Login() {
-    return createPortal(
-        //여기부터 JSX
-        <div className="popup">
-            <div className="popup_login">
-                <img src="logo_popup.png" />
-                <p className="logintext">로그인</p>
-                <p className="idtext">아이디</p><input className="inputid popupinput" type="text" placeholder=" 아이디를 입력하세요" />
-                <p className="pwtext">비밀번호</p><input className="inputpw popupinput" type="password" placeholder=" 비밀번호를 입력하세요" />
-                <button className="popupbtn" >로그인</button>
-            </div>
-        </div>,
-        //여기까지 JSX
-        document.getElementById("LoginPopup")
-    );
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+
+    handleJoin = () => {
+        fetch('/v1/member/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password,
+            }),
+        })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                if (response.success === true) {
+                    return alert('로그인에 성공하였습니다');
+                }
+
+                if (response.success !== true) {
+                    return alert('로그인에 실패하였습니다.');
+                }
+            });
+    };
+
+    handleInput = e => {
+        const { value, name } = e.target;
+        this.setState({ [name]: value });
+    };
+    render() {
+        return createPortal(
+            //여기부터 JSX
+            <div className="popup">
+                <div className="popup_login">
+                    <img src="logo_popup.png" />
+                    <p className="logintext">로그인</p>
+                    <p className="idtext">이메일</p><input id="email" name="email" className="popupinput" type="text" placeholder=" 이메일 주소를 입력하세요" onChange={this.handleInput} />
+                    <p className="pwtext">비밀번호</p><input id="password" name="password" className="popupinput" type="password" placeholder=" 비밀번호를 입력하세요" onChange={this.handleInput} />
+                    <button className="popupbtn" onClick={this.handleJoin}>로그인</button>
+                </div>
+            </div>,
+            //여기까지 JSX
+            document.getElementById("LoginPopup")
+        );
+    }
 }
 
 export default Login;
