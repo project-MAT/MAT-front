94623608a915941afa0d8a23cb3acd6ceff163fa
JYG <jojeayun900@gmail.com>
2021-12-02 20:34:05 +0900

    get Tokken

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { createPortal } from "react-dom";
 import { Component } from 'react';
+import axios from "axios";
 
 class Login extends Component {
     constructor(props) {
@@ -12,6 +13,7 @@ class Login extends Component {
     }
 
     handleJoin = () => {
+
         fetch('/v1/member/login', {
             method: 'POST',
             headers: {
@@ -24,13 +26,23 @@ class Login extends Component {
         })
             .then(response => response.json())
             .then(response => {
-                console.log(response);
-                if (response.success === true) {
-                    return alert('로그인에 성공하였습니다');
-                }
+                console.log(response.data);
+                const {Authorization}  = response.data.accessToken;
+                const {RefreshToken} = response.data.refreshToken;
+                console.log({Authorization});
+                console.log({RefreshToken});
+                // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+                axios.defaults.headers.common['Authorization'] = `Bearer ${Authorization}`;
+                axios.defaults.headers.common['RefreshToken'] = `Bearer ${RefreshToken}`;
 
+                console.log(Authorization);
+                if (response.success === true) {
+                    alert('로그인에 성공하였습니다');
+                    window.location.replace("/About");
+                }
                 if (response.success !== true) {
-                    return alert('로그인에 실패하였습니다.');
+                    alert('로그인에 실패하였습니다.');
+                    window.location.replace("/");
                 }
             });
     };
@@ -46,8 +58,8 @@ class Login extends Component {
                 <div className="popup_login">
                     <img src="logo_popup.png" />
                     <p className="logintext">로그인</p>
-                    <p className="idtext">이메일</p><input id="email" name="email" className="popupinput" type="text" placeholder=" 이메일 주소를 입력하세요" onChange={this.handleInput} />
-                    <p className="pwtext">비밀번호</p><input id="password" name="password" className="popupinput" type="password" placeholder=" 비밀번호를 입력하세요" onChange={this.handleInput} />
+                    <p className="L1 idtext">이메일</p><input id="email" name="email" className="L11 popupinput" type="text" placeholder=" 이메일 주소를 입력하세요" onChange={this.handleInput} />
+                    <p className="L2 pwtext">비밀번호</p><input id="password" name="password" className="L22 popupinput" type="password" placeholder=" 비밀번호를 입력하세요" onChange={this.handleInput} />
                     <button className="popupbtn" onClick={this.handleJoin}>로그인</button>
                 </div>
             </div>,
