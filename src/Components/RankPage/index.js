89c005b7b23fbe2bd41065513e7468cb2c559d11
yoosion030 <rsy5487@naver.com>
2022-02-09 10:43:11 +0900

    Add style

--- src/Components/RankPage/index.js
+++ src/Components/RankPage/index.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NotFoundPage from "../NotFoundPage";
+import "./style.css";
 
 const RankPage = () => {
   return (
-    <main>
+    <main className="MainSection">
       <div className="Select">좋아요를 많이 받은 구인공고</div>
+      <div className="Select">최근에 올라온 구인공고</div>
     </main>
   );
 };
