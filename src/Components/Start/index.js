ec8df9b2835746dad099a98c097583ff2e49dda8
JYG <jojeayun900@gmail.com>
2022-02-09 11:30:22 +0900

    Edit Page & Modals

--- src/Components/Start/index.js
+++ src/Components/Start/index.js
@@ -1,32 +1,37 @@
-import '../../App.css'
-import { LoginModal, LoginPage } from '../index'
+import '../Start/style.css'
 import React, { useState } from 'react'
+import LoginModal from "../Modals/LoginModal/index"
+import RegisterModal from '../Modals/RegisterModal'
 
 function Start() {
-  const [isLShowing, setLoginShowing] = useState(false)
+  const [loginShowing, setLoginShowing] = useState(false)
   const openLogin = () => {
     setLoginShowing(true)
   }
-  const [isRShowing, setRegisterShowing] = useState(false)
+  const closeLogin=(e) => {
+    setLoginShowing(false)
+  }
+
+  const [registerShowing, setRegisterShowing] = useState(false)
   const openRegister = () => {
     setRegisterShowing(true)
   }
+  const closeRegister=(e) => {
+    setRegisterShowing(false)
+  }
+
   return (
-    //여기부터 JSX
     <div className="App">
       <div className="start">
         <img className="logo" alt="MAT로고" src="logo.png" />
-        <button className="loginbtn btn" onClick={openLogin}>
-          로그인
-        </button>
-        <button className="registerbtn btn" onClick={openRegister}>
-          회원가입
-        </button>
+        <button className="login popupbtn" onClick={openLogin}>로그인</button>
+        <button className="register popupbtn" onClick={openRegister}>회원가입</button>
       </div>
-      <div>{isLShowing && <LoginPage />}</div>
-      <div>{isRShowing && <LoginModal />}</div>
+      {loginShowing && <LoginModal />}
+      {loginShowing && <LoginModal closeLogin={closeLogin} />}
+      {registerShowing && <RegisterModal/>}
+      {registerShowing && <RegisterModal closeRegister={closeRegister} />}
     </div>
-    //여기까지 JSX
   )
 }
 
