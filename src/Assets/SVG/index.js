38ee2eaca6c0fe83ce76473afb86e9d5e4831c64
yoosion030 <rsy5487@naver.com>
2022-02-09 22:18:04 +0900

    Add logo svg

--- src/Assets/SVG/index.js
+++ src/Assets/SVG/index.js
@@ -3,3 +3,4 @@ export { default as Write } from "./Write";
 export { default as Search } from "./Search";
 export { default as BlackLike } from "./BlackLike";
 export { default as RedLike } from "./RedLike";
+export { default as AboutLogo } from "./AboutLogo";
