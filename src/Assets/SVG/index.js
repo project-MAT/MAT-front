4d776e0467528769d4789b7f9749ca6e12bd3829
yoosion030 <rsy5487@naver.com>
2022-02-17 21:55:38 +0900

    Add NotFoundPage

--- src/Assets/SVG/index.js
+++ src/Assets/SVG/index.js
@@ -3,3 +3,4 @@ export { default as Write } from "./Write";
 export { default as Search } from "./Search";
 export { default as BlackLike } from "./BlackLike";
 export { default as RedLike } from "./RedLike";
+export { default as NotfoundLogo } from "./NotfoundLogo";
