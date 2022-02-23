f5f031014bf4f92b271a1a3e27772f26dace5316
yoosion030 <rsy5487@naver.com>
2022-02-23 21:21:19 +0900

    Update StartModal

--- src/Components/Modals/LoginModal/index.js
+++ src/Components/Modals/LoginModal/index.js
@@ -1,63 +1,75 @@
-import '../LoginModal/style.css'
-import { createPortal } from 'react-dom'
-import { Component } from 'react'
+import "../LoginModal/style.css";
+import { createPortal } from "react-dom";
+import { Component } from "react";
 
 class LoginModal extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      email: '',
-      password: '',
-    }
+      email: "",
+      password: "",
+    };
   }
 
   handleJoin = () => {
-    fetch('/auth/login', {
-      method: 'POST',
+    fetch("/auth/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: this.state.email,
         password: this.state.password,
       }),
     })
-      .then(response => response.json())
-      .then(response => {
-        console.log(response)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
         if (response.islogin === true) {
-          alert('로그인에 성공하였습니다')
-          window.location.replace('/')
+          alert("로그인에 성공하였습니다");
+          window.location.replace("/");
         }
         if (response.islogin !== true) {
-          alert('로그인에 실패하였습니다.')
-          window.location.replace('/')
+          alert("로그인에 실패하였습니다.");
+          window.location.replace("/");
         }
-      })
-  }
+      });
+  };
 
-  handleInput = e => {
-    const { value, id } = e.target
-    this.setState({ [id]: value })
-  }
+  handleInput = (e) => {
+    const { value, id } = e.target;
+    this.setState({ [id]: value });
+  };
 
   render() {
-    const {closeLogin} = this.props;
+    const { closeLogin } = this.props;
     return createPortal(
       <div className="modal" onClick={closeLogin}>
-        <div className="modal_login" onClick={e => e.stopPropagation()}>
+        <div className="modal_login" onClick={(e) => e.stopPropagation()}>
           <img className="modallogo" alt="MAT로고" src="logo.png" />
           <p className="logintext">로그인</p>
-          <p className="text">이메일</p>
-          <input id="email" className="modalinput" type="text" placeholder="이메일 주소를 입력하세요" onChange={this.handleInput} />
+          <p className="text">아이디</p>
+          <input
+            id="id"
+            className="modalinput"
+            type="text"
+            onChange={this.handleInput}
+          />
           <p className="text">비밀번호</p>
-          <input id="password" className="modalinput" type="password" placeholder="비밀번호를 입력하세요" onChange={this.handleInput} />
-          <button className="submitbtn" onClick={this.handleJoin}>로그인</button>
+          <input
+            id="password"
+            className="modalinput"
+            type="password"
+            onChange={this.handleInput}
+          />
+          <button className="submitbtn" onClick={this.handleJoin}>
+            로그인
+          </button>
         </div>
       </div>,
-      document.getElementById('LoginModal')
-    )
+      document.getElementById("LoginModal"),
+    );
   }
 }
 
-export default LoginModal
+export default LoginModal;
--- src/Components/Modals/RegisterModal/index.js
+++ src/Components/Modals/RegisterModal/index.js
@@ -1,25 +1,25 @@
-import '../RegisterModal/style.css'
-import { createPortal } from 'react-dom'
-import { Component } from 'react'
+import "../RegisterModal/style.css";
+import { createPortal } from "react-dom";
+import { Component } from "react";
 
 class RegisterModal extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      email: '',
-      name: '',
-      password: '',
-      cardinalNum: '',
-      majority: '',
-      discordNum: '',
-    }
+      email: "",
+      name: "",
+      password: "",
+      cardinalNum: "",
+      majority: "",
+      discordNum: "",
+    };
   }
 
   handleJoin = () => {
-    fetch('/v1/member/join', {
-      method: 'POST',
+    fetch("/v1/member/join", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: this.state.email,
@@ -30,51 +30,95 @@ class RegisterModal extends Component {
         discordNum: this.state.discordNum,
       }),
     })
-      .then(response => response.json())
-      .then(response => {
-        console.log(response)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
         if (response.success === true) {
-          alert('회원가입에 성공하였습니다')
-          window.location.replace('/')
+          alert("회원가입에 성공하였습니다");
+          window.location.replace("/");
         }
 
         if (response.success !== true) {
-          alert('회원가입에 실패하였습니다.')
-          window.location.replace('/')
+          alert("회원가입에 실패하였습니다.");
+          window.location.replace("/");
         }
-      })
-  }
+      });
+  };
 
-  handleInput = e => {
-    const { value, name } = e.target
-    this.setState({ [name]: value })
-  }
+  handleInput = (e) => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
-    const {closeRegister} = this.props;
+    const { closeRegister } = this.props;
     return createPortal(
       <div className="modal" onClick={closeRegister}>
-        <div className="modal_register" onClick={e => e.stopPropagation()}>
+        <div className="modal_register" onClick={(e) => e.stopPropagation()}>
           <img className="modallogo" alt="MAT로고" src="logo.png" />
           <p className="logintext">회원가입</p>
-          <p className="text">이메일</p>
-          <input name="email" className="modalinput" type="text" placeholder=" 이메일 주소를 입력하세요" onChange={this.handleInput} />
-          <p className="text">이름</p>
-          <input name="name" className="modalinput" type="text" placeholder=" 이름을 입력하세요" onChange={this.handleInput} />
-          <p className="text">비밀번호</p>
-          <input name="password" className="modalinput" type="password" placeholder=" 비밀번호를 입력하세요" onChange={this.handleInput} />
-          <p className="text">기수</p>
-          <input name="cardinalNum" className="modalinput" type="text" placeholder=" 기수를 입력하세요" onChange={this.handleInput} />
-          <p className="text">전공</p>
-          <input name="majority" className="modalinput" type="text" placeholder=" 전공을 입력하세요" onChange={this.handleInput} />
-          <p className="text">디스코드</p>
-          <input name="discordNum" className="modalinput" type="text" placeholder=" 디스코드 고유번호(#제외)를 입력하세요" onChange={this.handleInput} />
-          <button className="submitbtn" onClick={this.handleJoin}>회원가입</button>
+          <div>
+            <p className="text">이름</p>
+            <input
+              name="name"
+              className="modalinput"
+              type="text"
+              onChange={this.handleInput}
+            />
+          </div>
+          <div>
+            <p className="text">디스코드</p>
+            <input
+              name="discordNum"
+              className="modalinput"
+              type="text"
+              onChange={this.handleInput}
+            />
+          </div>
+          <div>
+            <p className="text">전공</p>
+            <input
+              name="majority"
+              className="modalinput"
+              type="text"
+              onChange={this.handleInput}
+            />
+          </div>
+          <div>
+            <p className="text">기수</p>
+            <input
+              name="cardinalNum"
+              className="modalinput"
+              type="text"
+              onChange={this.handleInput}
+            />
+          </div>
+          <div>
+            <p className="text">아이디</p>
+            <input
+              name="id"
+              className="modalinput"
+              type="text"
+              onChange={this.handleInput}
+            />
+          </div>
+          <div>
+            <p className="text">비밀번호</p>
+            <input
+              name="password"
+              className="modalinput"
+              type="password"
+              onChange={this.handleInput}
+            />
+          </div>
+          <button className="submitbtn" onClick={this.handleJoin}>
+            회원가입
+          </button>
         </div>
       </div>,
-      document.getElementById('RegisterModal')
-    )
+      document.getElementById("RegisterModal"),
+    );
   }
 }
 
-export default RegisterModal
+export default RegisterModal;
